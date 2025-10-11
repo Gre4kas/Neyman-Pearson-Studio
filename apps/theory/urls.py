abfbbb47c963a44365514ec2ed19219b8c2eb6b4
Gre4kas <misaa3818@gmail.com>
2025-10-11 19:27:20 +0300

    Add theory

--- apps/theory/urls.py
+++ apps/theory/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'theory'
+
 urlpatterns = [
-    path('', views.index, name='theory_index'),
+    path('', views.article_list_view, name='list'),
+    path('<slug:slug>/', views.article_detail_view, name='detail'),
 ]
