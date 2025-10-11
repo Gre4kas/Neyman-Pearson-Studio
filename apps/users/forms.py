251c2b8ff16ea1a5d9e25fe841e8f46e0974ef14
Gre4kas <misaa3818@gmail.com>
2025-10-11 22:55:20 +0300

    update auth

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -8,3 +8,22 @@ class UserRegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Add Bootstrap classes to all form fields
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control'
+            })
+        
+        # Update labels to Russian
+        self.fields['username'].label = 'Имя пользователя'
+        self.fields['email'].label = 'Email'
+        self.fields['password1'].label = 'Пароль'
+        self.fields['password2'].label = 'Подтверждение пароля'
+        
+        # Remove help text for cleaner look
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
